fab96503ee4492747346e314377a50a40e17e7e1
monster90xd-stack <monster.90xd@gmail.com>
2026-03-01 03:42:50 +0400

    Update cache name to gcc-gold-pwa-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gcc-gold-pwa-v1";
+const CACHE_NAME = "gcc-gold-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,8 @@ const ASSETS = [
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
+
+// rest of file unchanged
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
