cc87b6c4c3bb738d751e1d36576d8e57637b20bc
monster90xd-stack <monster.90xd@gmail.com>
2026-03-01 03:16:34 +0400

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "gcc-gold-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(caches.match(event.request).then((c) => c || fetch(event.request)));
+    return;
+  }
+
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
